b0a36aaf17ae2f24bd7a2a6430ef685ccd90a557
Matt Beaudoin <ehxtwo@gmail.com>
2021-04-28 22:06:12 -0700

    commit tree 85a0869be34da899a96a06ff8febc70f83ab6e61

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    temp_post = Post.new(post_params)
+    if temp_post.valid?
+      @post= Post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
